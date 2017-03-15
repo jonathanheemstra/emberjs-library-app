5e3a990668f2ceb908304620c13d1b554ae60042
Jonathan Heemstra <jonathan.heemstra@gmail.com>
2017-03-15 16:14:15 -0700

    validate that an email has been entered

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  isDisabled: true
+  emailAddress: '',
+  isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
+  isDisabled: Ember.computed.not('isValid'),
+  emailAddressChanged: Ember.observer('emailAddress', function() {
+    console.log('observer is called', this.get('emailAddress'));
+  }),
+  actualEmailAddress: Ember.computed('emailAddress', function() {
+    console.log('actualEmailAddress function is called: ', this.get('emailAddress'));
+  }),
+
+  // **** Alternative longhanded ways of determining if a form is filled in or not ****
+  // isDisabled: Ember.computed.empty('emailAddress'),
+  // isDisabled: Ember.computed('emailAddress', function() {
+  //   return this.get('emailAddress') === '';
+  // })
 });
