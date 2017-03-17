52a96602b2344973765c064c94eda265468b2b14
Jonathan Heemstra <jonathan.heemstra@gmail.com>
2017-03-16 17:46:47 -0700

    add delete methods to admin area

--- app/routes/admin/invitations.js
+++ app/routes/admin/invitations.js
@@ -2,7 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    console.log('this.store.findAll', this.store.findAll('invitation'));
     return this.store.findAll('invitation');
+  },
+
+  actions: {
+    deleteInvitation(invitation) {
+      let confirmation = confirm('Are you sure you?');
+
+      if(confirmation) {
+        invitation.destroyRecord();
+      }
+    }
   }
 });
