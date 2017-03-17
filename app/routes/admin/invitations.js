34fb316840baea54ce3ec80ab5e872b267138f67
Jonathan Heemstra <jonathan.heemstra@gmail.com>
2017-03-16 17:29:06 -0700

    add edit route to libraries

--- app/routes/admin/invitations.js
+++ app/routes/admin/invitations.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
+    console.log('this.store.findAll', this.store.findAll('invitation'));
     return this.store.findAll('invitation');
   }
 });
